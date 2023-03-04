c23d2102b9eb13892c9dd39d19a5b4c6b3db065c
victojoy <victor_maslov1337@mail.ru>
2023-03-04 12:05:40 +0700

    SplashScreen

--- app/src/main/java/com/example/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.example.dictionary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,Intro1Activity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
 }
