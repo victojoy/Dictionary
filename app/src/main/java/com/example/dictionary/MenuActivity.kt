2c6387313639222339aa8b417b22d32bf3969bed
victojoy <victor_maslov1337@mail.ru>
2023-03-06 21:37:06 +0700

    Dictionary_navigation_bar

--- app/src/main/java/com/example/dictionary/MenuActivity.kt
+++ app/src/main/java/com/example/dictionary/MenuActivity.kt
@@ -2,10 +2,44 @@ package com.example.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.dictionary.databinding.ActivityMenuBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MenuActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigation : BottomNavigationView
+    private lateinit var binding : ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        loadFragment(DictionaryFragment())
+        bottomNavigation = binding.bottomNavigation
+
+        bottomNavigation.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.dictionary -> {
+                    loadFragment(DictionaryFragment())
+                    true
+                }
+                R.id.training -> {
+                    loadFragment(TrainingFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(VideoFragment())
+                    true
+                }
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
